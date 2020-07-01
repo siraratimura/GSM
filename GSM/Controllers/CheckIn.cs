b472fc94da048b959909fe12607289356e2c0831
Sirarat Somsak <Sirarat@SRV_Domain.Local>
2020-07-01 14:12:55 +0700

    Add project files.

---
+++ GSM/Controllers/CheckIn.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GSM.Controllers
+{
+    public class CheckIn : Controller
+    {
+
+        public IActionResult Testarea()
+        {
+            return View();
+        }
+
+        public IActionResult CI_Identify()
+        {
+            return View();
+        }
+
+        public IActionResult Book_Info()
+        {
+            return View();
+        }
+
+        public IActionResult CI_Payment()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ GSM/Controllers/CheckOut.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GSM.Controllers
+{
+    public class CheckOut : Controller
+    {
+
+        public IActionResult CO_Identify()
+        {
+            return View();
+        }
+
+        public IActionResult CO_Folio()
+        {
+            return View();
+        }
+
+        public IActionResult CO_NewPosting()
+        {
+            return View();
+        }
+
+        public IActionResult CO_Transfer()
+        {
+            return View();
+        }
+
+        public IActionResult CO_Payment()
+        {
+            return View();
+        }
+    }
+}
